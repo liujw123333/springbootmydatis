45944a7f8d4fdc21c9025eae3d789448cd2959d0
liujw <liujw@163.com>
2019-12-14 17:26:35 +0800

    Initial commit

---
+++ src/main/java/com/demo3/springbootmydatis/SpringbootmydatisApplication.java
@@ -0,0 +1,13 @@
+package com.demo3.springbootmydatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmydatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmydatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo3/springbootmydatis/SpringbootmydatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo3.springbootmydatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmydatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
